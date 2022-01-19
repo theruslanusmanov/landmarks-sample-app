621a017ec5417f9eb30a3ea7d95956b63611d13b
Ruslan Usmanov <ruslanusm6@gmail.com>
2022-01-19 21:50:43 +0500

    Initial Commit

---
+++ Shared/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Shared
+//
+//  Created by Ruslan Usmanov on 19.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
