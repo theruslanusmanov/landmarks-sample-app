462336d12ff05b369af031d2595ffa9f4e4b862e
Ruslan Usmanov <ruslanusm6@gmail.com>
2022-02-15 22:35:48 +0500

    add a category view

---
+++ Views/Categories/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  Landmarks (iOS)
+//
+//  Created by Ruslan Usmanov on 15.02.2022.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
