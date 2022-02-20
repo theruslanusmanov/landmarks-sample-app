6f6ebf948202e6beb398f52ead59d2a26a8fd135
Ruslan Usmanov <ruslanusm6@gmail.com>
2022-02-20 22:38:37 +0500

    display a user profile

---
+++ Views/Profiles/ProfileHost.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileHost.swift
+//  Landmarks (iOS)
+//
+//  Created by Ruslan Usmanov on 20.02.2022.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            ProfileSummary(profile: draftProfile)
+        }
+        .padding()
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+    }
+}
